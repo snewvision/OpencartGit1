7cd5f2f3be9eb9f8d52c0d87a77e1c1b33dc1159
deepanshusahu127 <deepanshu.sahu@newvision-software.com>
2026-05-20 10:39:48 +0530

    fixtureupdate

--- src/fixture/fixture.ts
+++ src/fixture/fixture.ts
@@ -1,9 +1,12 @@
 import { test as base, expect } from "@playwright/test";
 import { registerAction } from "../action/registerAction";
 import testdata from "../testdata/register.json";
+import { Addtocart } from "../page/addtocartPage";
+import { CartAction } from "../action/addtocartAction";
 
 type appActions = {
     register: registerAction;
+    addtocart: CartAction;
 }
 
 type Fixtures = {
@@ -13,7 +16,8 @@ type Fixtures = {
 export const test = base.extend<Fixtures>({
     appActions: async ({page}, use) => {
         const appActions: appActions = {
-            register : new registerAction(page)
+            register : new registerAction(page),
+            addtocart: new CartAction(new Addtocart(page)),
         }
         await use(appActions);
     }
