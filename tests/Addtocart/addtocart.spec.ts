c462d33dcc67e64652957538b3307cb41a21fffa
Shubham Nigam <shubham.nigam@newvision-software.com>
2026-05-21 13:54:19 +0530

    new updated

--- tests/Addtocart/addtocart.spec.ts
+++ tests/Addtocart/addtocart.spec.ts
@@ -7,7 +7,7 @@ test("Add to cart", async ({ page ,appActions}) =>
   await appActions.addtocart.searchProduct(loginData.SearchProduct)
 });
 
-test.only("Search product2 and apply filters", async ({ page ,appActions}) =>{
+test("Search product2 and apply filters", async ({ page ,appActions}) =>{
   await page.goto(loginData.baseURL)
   await appActions.addtocart.searchProduct2(loginData.SearchProduct2);
 });
